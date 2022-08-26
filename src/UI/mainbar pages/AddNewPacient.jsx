2cf9f3e4c379a0496d917d650650714f3620ee52
facudam <facundo89caceres@gmail.com>
2022-08-26 20:57:26 -0300

    add formulario agregar paciente, estilos de formulario

--- src/UI/mainbar pages/AddNewPacient.jsx
+++ src/UI/mainbar pages/AddNewPacient.jsx
@@ -10,9 +10,24 @@ export const AddNewPacient = () => {
 
   return (
     <div className="add-page">
-      AddNewPacient
-      
+      <p className='add-page__paragraph'>Ingrese los datos del nuevo paciente:</p>
 
+      <form className='datos-personales-container'>
+        <label for='nombre'>Nombre: </label>
+        <input type='text' id='nombre' placeholder='Ingrese aquí los nombres' />
+
+        <label for='apellido'>Apellido: </label>
+        <input type='text' id='apellido' placeholder='Ingrese aquí el apellido' />
+        
+        <label for='dni'>DNI: </label>
+        <input type='number' placeholder='Ingrese el número de documento' />
+
+        <label for='edad'>Edad: </label>
+        <input type='number' placeholder='Ingresa su edad' /> 
+
+        <button type='submit'>Añadir</button>
+      </form>
+      
     </div>
   )
 }
