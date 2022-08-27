f941db4c94d754087463ec9603a7a35858e2db0b
facudam <facundo89caceres@gmail.com>
2022-08-27 20:17:06 -0300

    Handles inputs added para creación de nuevo paciente

--- src/UI/mainbar pages/AddNewPacient.jsx
+++ src/UI/mainbar pages/AddNewPacient.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Context } from '../../context/Context';
 import '../../UI styles/AddNewPacient.css';
 
@@ -6,26 +6,57 @@ export const AddNewPacient = () => {
 
   const { pacientes} = useContext(Context)
 
-  console.log(pacientes)
+  const [ nombre, setNombre ] = useState('');
+  const [ apellido, setApellido ] = useState('');
+  const [ dni, setDni ] = useState('');
+  const [ edad, setEdad ] = useState('');
+
+  const handleName = (e) => {
+    setNombre(e.target.value)
+  }
+
+  const handleApellido = (e) => {
+    setApellido(e.target.value)
+  }
+
+  const handleDni = (e) => {
+    setDni(e.target.value)
+    
+  }
+
+  const handleEdad = (e) => {
+    setEdad(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  
+
+  
 
   return (
     <div className="add-page">
       <p className='add-page__paragraph'>Ingrese los datos del nuevo paciente:</p>
 
       <form className='datos-personales-container'>
-        <label for='nombre'>Nombre: </label>
-        <input type='text' id='nombre' placeholder='Ingrese aquí los nombres' />
+        <label htmlFor='nombre'>Nombre: </label>
+        <input type='text' value={ nombre } onChange={ handleName } id='nombre' placeholder='Ingrese aquí los nombres' />
 
-        <label for='apellido'>Apellido: </label>
-        <input type='text' id='apellido' placeholder='Ingrese aquí el apellido' />
+        <label htmlFor='apellido'>Apellido: </label>
+        <input type='text' value={ apellido } onChange={ handleApellido } id='apellido' placeholder='Ingrese aquí el apellido' />
         
-        <label for='dni'>DNI: </label>
-        <input type='number' placeholder='Ingrese el número de documento' />
+        <label htmlFor='dni'>DNI: </label>
+        <input type='number' value={ dni } onChange={ handleDni } placeholder='Ingrese el número de documento' />
 
-        <label for='edad'>Edad: </label>
-        <input type='number' placeholder='Ingresa su edad' /> 
+        <label htmlFor='edad'>Edad: </label>
+        <input type='number' value={ edad } onChange={ handleEdad } placeholder='Ingresa su edad' /> 
 
-        <button type='submit'>Añadir</button>
+        <button 
+          type='submit'
+          onClick={ onSubmit }
+        >Añadir</button>
       </form>
       
     </div>
