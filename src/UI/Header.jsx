8b37ca127f5be54d054db57a0dcc91505b95f3e0
facudam <facundo89caceres@gmail.com>
2022-11-19 18:44:51 -0300

    Solucionando desplazamiento horizontal en mobile

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -7,10 +7,8 @@ export const Header = () => {
         <div className='header'>
 
           <div className='header__logo-container'>
-            <img className="header__img" src='https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png' alt='logo' />
-            <span className='header__span-name'>Ficha Médica APP</span>
+            <div className='header__div-name'>FICHAS MÉDICAS <span>APP</span></div>
           </div>
-
           
           <nav>
               <ul>
