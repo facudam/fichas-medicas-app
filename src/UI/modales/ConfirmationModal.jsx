04b4af67de81641d9e501bb7b28d78a5d2752c5a
facudam <facundo89caceres@gmail.com>
2023-05-14 22:04:20 -0300

    estilando confirmation modal

--- src/UI/modales/ConfirmationModal.jsx
+++ src/UI/modales/ConfirmationModal.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import ReactDOM from "react-dom";
 import { Context } from "../../context/Context";
+import '../../UI styles/Modal.css'
 
 
 export const ConfirmationModal = ({ paciente, nombrePaciente }) => {
@@ -11,9 +12,10 @@ export const ConfirmationModal = ({ paciente, nombrePaciente }) => {
 
     return ReactDOM.createPortal(
         <>
-            <div className="fondo-confirmation"></div>
+            {/* <div className="fondo-agregar"></div> */}
+            <div className="modal-fondo2"></div>
             <div className="confirmation-modal">
-                <p className="mensaje">¿Está seguro que desea eliminar al paciente { nombrePaciente }?</p>
+                <p className="mensaje">¿Está seguro que desea eliminar al paciente { nombrePaciente } { paciente.apellido}?</p>
                 <div className="btn-container">
                     <button
                         onClick= { () => deletePatient(paciente) }
