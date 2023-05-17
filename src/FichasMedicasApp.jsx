cd8d9c8d37207fdb2fee5c65036b20d6640e73c4
facudam <facundo89caceres@gmail.com>
2023-05-17 20:38:41 -0300

    Agregando estilo en sidebar segun la ruta

--- src/FichasMedicasApp.jsx
+++ src/FichasMedicasApp.jsx
@@ -1,15 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from './UI/Header';
 import { AddNewPatient } from './UI/mainbar pages/AddNewPatient';
-import { FichaActiva } from './UI/mainbar pages/FichaActiva';
 import { Patients } from './UI/mainbar pages/Patients';
 import { Sidebar } from './UI/Sidebar';
 
 export const FichasMedicasApp = () => {
   return (
     <>
-      
-
       <div className='body-app'>
         <Header />
         <Sidebar />
@@ -17,12 +14,10 @@ export const FichasMedicasApp = () => {
         <Routes>
           <Route path='/' element={ <Patients /> }/>
           <Route path='/agregar-paciente' element={ <AddNewPatient /> } />
-          <Route path='/ficha-activa' element={<FichaActiva />} />
 
           <Route path='/*' element={ <Navigate to='/'/> } />
         </Routes>
       </div>
     </> 
- 
   )
 }
--- src/UI/Sidebar.jsx
+++ src/UI/Sidebar.jsx
@@ -1,21 +1,28 @@
-import { Link } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import '../UI styles/Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHospitalUser, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import user from '../user.jpg';
 
 
 export const Sidebar = () => {
+  const location = useLocation();
+
   return (
     <nav className="sidebar">
-        <Link className='link' to="/">
+        <div className='admin'>
+          <img src={user} alt='profile-admin' className='admin-img'/>
+          <p>Administrador</p>
+        </div>
+        <NavLink className={`link ${location.pathname === '/' ? 'link-activo' : ''}`} to="/" >
           <FontAwesomeIcon icon={faHospitalUser} className='font-icon' />
           Pacientes
-        </Link>
+        </NavLink>
         
-        <Link className='link' to="/agregar-paciente">
+        <NavLink className={`link ${location.pathname === '/agregar-paciente' ? 'link-activo' : ''}`} to="/agregar-paciente">
           <FontAwesomeIcon icon={faUserPlus} className='font-icon' />
           Ingresar nuevo
-        </Link>
+        </NavLink>
       
            
     </nav>
