3db7b794fc7d3379c10e8f9255ee7f87a830b912
facudam <facundo89caceres@gmail.com>
2023-06-12 20:12:44 -0300

    Debuggeando estilos

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -4,17 +4,10 @@ import '../UI styles/Header.css';
 export const Header = () => {
   return (
     <header>
-        <div className='header'>
-
-          <div className='header__logo-container'>
-            <div className='header__div-name'>FICHAS MÉDICAS <span>APP</span></div>
-          </div>
-          
-          <nav>
-              <ul>
-                    
-              </ul>
-          </nav>
+        <div className='header__logo-container'>
+            <div className='header__div-name'>
+              FICHAS MÉDICAS <span>APP</span>
+            </div>
         </div>
     </header>
   )
